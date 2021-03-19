0176c295b49cf8a81fd7816812181bc1b80b9e1c
Kalan Prudhomme <cleaver9@gmail.com>
2021-03-19 13:19:24 -0700

    added drop down selector and simplified calculator

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
-// import functions and grab DOM elements
-//Future drop down implementation
-//const computeButton = document.getElementById('calculate');
-//const digitOne = document.getElementById('input1');
-//const digitTwo = document.getElementById('input2');
-//const operatorOne = document.getElementById('a');
-//const operatorTwo = document.getElementById('b');
-//const operatorThree = document.getElementById('c');
-//const operatorFour = document.getElementById('d');
-//const answer = document.getElementById('solution');
 //Imported Functions
 
 import { handleAddClick, handleSubClick, handleMultClick, handleDivClick } from './handlers.js';
-
+//Addition
 const addButton = document.getElementById('add-button');
 //Subtraction
-
 const subButton = document.getElementById('subtract-button');
 
 //Multiplication
-
 const multButton = document.getElementById('multiply-button');
 
 //Division
-
 const divButton = document.getElementById('divide-button');
 
 // set event listeners to update state and DOM
@@ -31,15 +18,36 @@ addButton.addEventListener('click', handleAddClick);
 subButton.addEventListener('click', handleSubClick);
 multButton.addEventListener('click', handleMultClick);
 divButton.addEventListener('click', handleDivClick);
-   
-//computeButton.addEventListener('click', () => {
-//    const x = digitOne.valueAsNumber;
-//    const y = digitTwo.valueAsNumber;
-//
-//    if (mainOperator = operatorOne) {
-//   const solution = x + y;
-//    answer.textContent = solution;
-//}
-//
-//});
 
+//Simpler Calculator
+
+const computeButton = document.getElementById('calculate');
+const digitOne = document.getElementById('input1');
+const digitTwo = document.getElementById('input2');
+const answer = document.getElementById('solution');
+const select = document.getElementById('selector');
+
+let operator = '+';
+select.addEventListener('change', (e) => {
+    operator = e.target.value;
+});
+
+computeButton.addEventListener('click', () => {
+    const x = digitOne.valueAsNumber;
+    const y = digitTwo.valueAsNumber;
+    if (operator === '+') {
+        const solution = x + y;
+        answer.textContent = solution;
+    } else if (operator === '-') {
+        const solution = x - y;
+        answer.textContent = solution;
+    } else if (operator === 'x') {
+        const solution = x * y;
+        answer.textContent = solution;
+    } else if (operator === '/') {
+        const solution = x / y;
+        answer.textContent = solution;
+    }
+}
+);
+    
